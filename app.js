f75c0af8f51dcd45145cd5509a5f49e615995888
Suyash Joshi <suyash@suyashjoshi.com>
2026-09-23 15:44:05 +0100

    updated project

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/hello', (req, res) => {
   res.json({ ok: true });
 });
@@ -17,4 +19,4 @@ app.get('/oops', (req, res) => {
   throw new Error('something went wrong');
 });
 
-app.listen(3000, () => console.log('weather-demo listening on http://localhost:3000'));
+app.listen(3000, () => console.log('hello-edot-node listening on http://localhost:3000'));
